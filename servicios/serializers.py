47002a2eb999fe1577d54981e12ff747d15365a5
isaac tacsa <112971006+Zanaborio21@users.noreply.github.com>
2022-12-29 18:23:20 -0500

    Delete servicios directory

--- servicios/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from.models import Service
-
-class ServicioSerializer(serializers.ModelSerializer):
-        class Meta:
-            model=Service
-            fields = '__all__'
-            #read_only_fields = '__all__',
