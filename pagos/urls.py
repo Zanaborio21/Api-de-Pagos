5c5562d76730bf3e21788e4c80d0e896109be145
isaac tacsa <112971006+Zanaborio21@users.noreply.github.com>
2022-12-29 18:23:00 -0500

    Delete pagos directory

--- pagos/urls.py
+++
@@ -1,9 +0,0 @@
-from .api import Pago_ExpiradoView,PagoView
-from rest_framework import routers
-
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register(r'pago', PagoView, 'pago')
-router.register(r'pago_expirado', Pago_ExpiradoView, 'pago_expirado')
-urlpatterns=router.urls
